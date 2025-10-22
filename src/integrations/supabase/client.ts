3c76540719216120d48416123283d3528f05913b
lexam37 <64509075+lexam37@users.noreply.github.com>
2025-10-22 14:32:34 +0200

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || 'https://mzffarvnlbtylnulkmil.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16ZmZhcnZubGJ0eWxudWxrbWlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2MDY5MTYsImV4cCI6MjA3NjE4MjkxNn0.yhKshNZXCw1lbZl9eJzNfLf1OiaJNM8nslH7KGUHqFs';
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
