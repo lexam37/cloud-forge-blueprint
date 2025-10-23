ee18164d53403173446d462c397185aa817c656f
lexam37 <64509075+lexam37@users.noreply.github.com>
2025-10-23 15:20:19 +0200

    Update index.ts

--- supabase/functions/analyze-template/index.ts
+++ supabase/functions/analyze-template/index.ts
@@ -2,6 +2,9 @@ import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 import { convert } from "https://esm.sh/mammoth@1.6.0";
 
+const supabaseUrl = Deno.env.get('SUPABASE_URL')!;
+const supabaseKey = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')!;
+const supabase = createClient(supabaseUrl, supabaseKey);
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
