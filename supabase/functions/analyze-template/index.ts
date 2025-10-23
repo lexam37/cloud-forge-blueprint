08d44433e74f8309e322fd44c7f9323bd2f8b146
lexam37 <64509075+lexam37@users.noreply.github.com>
2025-10-23 15:28:21 +0200

    Update index.ts

--- supabase/functions/analyze-template/index.ts
+++ supabase/functions/analyze-template/index.ts
@@ -1,5 +1,5 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2.75.0";
 import { convert } from "https://esm.sh/mammoth@1.6.0";
 
 const supabaseUrl = Deno.env.get('SUPABASE_URL')!;
